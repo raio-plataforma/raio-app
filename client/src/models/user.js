658f204658dd2a545a07b0c870072734df95b3ae
Fabi <frontfabi@gmail.com>
2020-03-15 14:21:55 -0300

    [ADD] User Update

--- client/src/models/user.js
+++ client/src/models/user.js
@@ -1,4 +1,5 @@
 import { thunk, action } from 'easy-peasy'
+import history from '../history'
 import axios from 'axios'
 
 const userModel = {
@@ -17,6 +18,35 @@ const userModel = {
     }
     catch (e) {
       throw e
+    }
+  }),
+  getUserById: thunk(async (actions, payload) => {
+    try {
+      const user = await axios.get(`/api/user/${payload}`)
+      // Set current user profile
+      
+      actions.setUser({
+        id: user.data._id,
+        name: user.data.name,
+        email: user.data.email,
+        phone: user.data.phone,
+        gender: user.data.gender,
+        type: user.data.type,
+        self_declaration: user.data.self_declaration
+      })
+    }
+    catch (e) {
+      actions.setError(e)
+    }
+  }),
+  editUser: thunk(async (actions, payload) => {
+    try {
+      await axios.put(`/api/user/edit/${payload.type}/${payload.id}`, payload)
+      const type = payload.type === 'enterprise' ? 'empresa' : 'profissional'
+      return history.push(`/dashboard/${type}`)
+    }
+    catch (e) {
+      actions.setError(e)
     }
   }),
   getAllUsers: thunk(async (actions) => {
