4d686e4468f06822cbdd7c2e93665af2a63fe497
Fabi <frontfabi@gmail.com>
2020-04-05 16:25:07 -0300

    segments

--- client/src/pages/Dashboard/Profile.jsx
+++ client/src/pages/Dashboard/Profile.jsx
@@ -79,7 +79,7 @@ function Profile({ id, icon, name, associate, bio, type, segments, pcd }) {
       {segments && segments.length > 0 &&
       <section className="segments">
         <Typography component="h3" variant="h6">Segmentos de atuação</Typography>
-        <Typography color="secondary">{getKeys(segments[0]) || 'Nenhum'}</Typography>
+        <Typography color="secondary">{segments.join(', ') || 'Nenhum'}</Typography>
       </section>}
 
       <Link 
