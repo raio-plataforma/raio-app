f3090405f280295a7ade743966075dce5b57467a
Diogo Abdalla <diogo.abdalla@gmail.com>
2020-07-10 13:38:31 -0300

    redir back

---
+++ client/build/precache-manifest.0a0d19a0359c5c554c32be59aa474830.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdfde9dc62a39a2622a8c7699c11c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "649ad1d3b8b3a012050c",
+    "url": "/static/css/main.22c65781.chunk.css"
+  },
+  {
+    "revision": "f48cfc29cacb0c124a77",
+    "url": "/static/js/2.df56e2bf.chunk.js"
+  },
+  {
+    "revision": "99eed7147ea5795708cb9270d93db0ae",
+    "url": "/static/js/2.df56e2bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "649ad1d3b8b3a012050c",
+    "url": "/static/js/main.ffbaf832.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "3b4e7be440e9bdf66e9dd5e9ae5a0388",
+    "url": "/static/media/loading.3b4e7be4.svg"
+  },
+  {
+    "revision": "345f5cb3d72b2a9c69cdde95f59dff4b",
+    "url": "/static/media/raio_bemvindo.345f5cb3.png"
+  },
+  {
+    "revision": "c1265b6ae07514d5b7a9f122b541344f",
+    "url": "/static/media/selo.c1265b6a.png"
+  }
+]);
